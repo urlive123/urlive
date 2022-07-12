1c2edd6208e72adcda126999b7e85e42f6aa8b8f
AnnieHa1002 <108536712+AnnieHa1002@users.noreply.github.com>
2022-07-12 19:52:34 +0900

    하트순 누르면 하트수 되는 오류 수정

--- app.py
+++ app.py
@@ -196,14 +196,19 @@ def api_get_by_comment():
 
 @app.route('/api/sort_heart', methods=['GET'])
 def card_sort_heart():
+    token_receive = request.cookies.get('mytoken')
+    payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
     content_sort_heart = list(db.urliveContents.find())
     print(content_sort_heart)
     for document in content_sort_heart:
         document['_id'] = str(document['_id'])
-        document['count_heart'] = db.urliveLikes.count_documents({"num": str(document['_id'])})
-    content_sort_heart.sort(key=lambda content: content["count_heart"],reverse=True)
+        document["count_heart"] = db.urliveLikes.count_documents({"post_id": document["_id"], "type": "heart"})
+        document["heart_by_me"] = bool(
+            db.urliveLikes.find_one({"post_id": document["_id"], "type": "heart", "id": payload['id']}))
+    content_sort_heart.sort(key=lambda content: content["count_heart"], reverse=True)
     print(content_sort_heart)
     return jsonify({'contents': content_sort_heart})
+
 
 
 if __name__ == '__main__':
