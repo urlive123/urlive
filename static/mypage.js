791d0851cdb5cb5ea16f4ef0f423c85c1bb33667
Kim Hyeon seo <rlagustj77@naver.com>
2022-07-12 21:51:57 +0900

    no message

--- static/mypage.js
+++ static/mypage.js
@@ -5,6 +5,12 @@ $(document).ready(function () {
 
 });
 
+function youtube_parser(url) {
+    let regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+    let match = url.match(regExp);
+    return (match && match[7].length == 11) ? match[7] : false;
+}
+
 function showMyLike() {
     $.ajax({
         type: "GET",
