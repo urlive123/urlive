fa8fa8b4b80166c67717984406f90780a156dd07
Kim Hyeon seo <rlagustj77@naver.com>
2022-07-14 10:09:33 +0900

    no message

--- static/login.js
+++ static/login.js
@@ -30,22 +30,30 @@ function login() {
 function register() {
     id = $('#register-id').val();
     pw = $('#register-password').val();
-    if(id == "" || pw == "") {
+    pw_check = $('#register-password-check').val();
+    let idReg = /^[a-zA-Z]+[a-z0-9A-Z]{3,19}$/g;
+    if (!idReg.test(id)) {
+        alert("아이디는 영소문자로 시작하는 4~20자 영문자 또는 숫자이어야 합니다.");
+        return;
+    }
+    if (id == "" || pw == "") {
         alert('아이디와 비밀번호를 정확히 입력해주세요!')
+    } else if (pw != pw_check) {
+        alert('동일한 비밀번호를 입력해주세요.')
     } else {
-    $.ajax({
-        type: "POST",
-        url: "/api/sign-up",
-        data: {id_give: id, pw_give: pw},
-        success: function (response) {
-            if(response['check'] == 1) {
-                alert('회원가입 성공!')
-                window.location.replace("/main")
-            } else if(response['check'] == 0 ){
-                alert('이미 가입한 회원입니다.')
+        $.ajax({
+            type: "POST",
+            url: "/api/sign-up",
+            data: {id_give: id, pw_give: pw},
+            success: function (response) {
+                if (response['check'] == 1) {
+                    alert('회원가입 성공!')
+                    window.location.replace("/main")
+                } else if (response['check'] == 0) {
+                    alert('이미 가입한 회원입니다.')
+                }
             }
-        }
-    })
+        })
     }
 }
 
